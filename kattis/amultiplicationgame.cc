3b263854537d76b22484722e9685f39d9aeb503a
Howard Cheng <howard.cheng@uleth.ca>
2020-10-30 21:04:48 -0600

    amultiplicationgame

---
+++ kattis/amultiplicationgame.cc
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* indexed by powers of 2, 3, 5, 7 */
+int table[33][22][15][13];
+
+void filltable(long long n, long long p, int e1, int e2, int e3, int e4)
+{
+  int a1, a2, a3, a4;
+  if (table[e1][e2][e3][e4]) {
+    return;
+  }
+
+  if (p*9 >= n) {
+    table[e1][e2][e3][e4] = 1;
+    return;
+  }
+
+  a1 = e1+1; a2 = e2; a3 = e3; a4 = e4;
+  filltable(n, p*2, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1; a2 = e2+1; a3 = e3; a4 = e4;
+  filltable(n, p*3, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1+2; a2 = e2; a3 = e3; a4 = e4;
+  filltable(n, p*4, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1; a2 = e2; a3 = e3+1; a4 = e4;
+  filltable(n, p*5, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1+1; a2 = e2+1; a3 = e3; a4 = e4;
+  filltable(n, p*6, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1; a2 = e2; a3 = e3; a4 = e4+1;
+  filltable(n, p*7, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1+3; a2 = e2; a3 = e3; a4 = e4;
+  filltable(n, p*8, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  a1 = e1; a2 = e2+2; a3 = e3; a4 = e4;
+  filltable(n, p*9, a1, a2, a3, a4);
+  if (-table[a1][a2][a3][a4] > table[e1][e2][e3][e4]) {
+    table[e1][e2][e3][e4] = -table[a1][a2][a3][a4];
+    return;
+  }
+  table[e1][e2][e3][e4] = -1;
+}
+
+int main(void)
+{
+  long long n;
+  int i, j, k, l;
+
+  while (cin >> n) {
+    /* clear table */
+    for (i = 0; i < 33; i++)
+      for (j = 0; j < 22; j++)
+	for (k = 0; k < 15; k++)
+	  for (l = 0; l < 13; l++)
+	    table[i][j][k][l] = 0;
+
+    filltable(n, 1, 0, 0, 0, 0);
+    if (table[0][0][0][0] == 1) {
+      cout << "Stan wins." << endl;
+    } else {
+      cout << "Ollie wins." << endl;
+    }
+  }
+  return 0;
+}
