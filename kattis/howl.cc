fdcdaae1411141761eb4b325b3d02e789fecc1d6
Howard Cheng <howard.cheng@uleth.ca>
2021-01-19 09:53:16 -0700

    howl

---
+++ kattis/howl.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  for (int i = 0; i < s.length(); i++) {
+    cout << 'A';
+  }
+  cout << "WHO" << endl;
+  return 0;
+}
