976143dd2df54c0fd0c3a8f9f0a45039a7fb34e0
Howard Cheng <howard.cheng@uleth.ca>
2021-09-22 10:52:12 -0600

    soylent

---
+++ kattis/soylent.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int T;
+  cin >> T;
+  while (T--) {
+    int N;
+    cin >> N;
+
+    cout << (N + 399) / 400 << endl;
+
+  }
+  return 0;
+}
