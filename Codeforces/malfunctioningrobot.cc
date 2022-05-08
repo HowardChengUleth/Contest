0a6fcdbc89be077f78f2a85e12825fc2813749c4
Howard Cheng <howard.cheng@uleth.ca>
2022-05-07 22:42:46 -0600

    malfunctioningrobot

--- Codeforces/malfunctioningrobot.cc
+++ Codeforces/malfunctioningrobot.cc
@@ -10,13 +10,14 @@ void solve()
   long long dx = abs(x1-x2), dy = abs(y1-y2);
   if (dx < dy) swap(dx, dy);
 
-  if (dx == 0) {
-    cout << 0 << endl;
-    return;
-  }
-  
   // number of R == dx
   // number of U/D: U - D == dy, U + D == dx or U + D == dx-1 depending on parity of dx+dy
+  //
+  // in the first case, U = (dx+dy)/2 and D = (dx-dy)/2 >= 0 because dx >= dy
+  // in the second case, U = (dx-1 + dy)/2 and D = (dx-1-dy)/2 >= 0, because dx > 0 if dx + dy is
+  //   odd
+  // Note that this works even if dx == 0, because dy will also be 0 and we are in the even case.
+  
   long long ans = dx;
   if ((dx + dy) % 2 == 0) {
     ans += dx;
