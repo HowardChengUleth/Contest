fd797e582d073a7dd7c235794945a28d5a8b56cf
Howard Cheng <howard.cheng@uleth.ca>
2022-12-06 22:40:40 -0700

    Day 7 AOC 2022

---
+++ adventofcode/2022/7b.cc
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+  string name;
+  bool is_dir;
+  int size;
+  vector<Node *> child;
+
+  Node(string N)
+    : name(N), is_dir(true) { }
+  Node(string N, int S)
+    : name(N), is_dir(false), size(S) { }
+
+  ~Node()
+  {
+    for (auto p : child) {
+      delete p;
+    }
+  }
+
+  void print() const
+  {
+    cout << name << ' ';
+    if (is_dir) {
+      cout << "dir";
+    } else {
+      cout << size;
+    }
+  }
+};
+
+Node *parse(string curr = "/")
+{
+  Node *root = new Node(curr);
+		    
+  string line;
+  getline(cin, line);
+  assert(line == "$ ls");
+
+  while (getline(cin, line) && line != "$ cd ..") {
+    istringstream iss(line);
+    string first, second;
+    iss >> first >> second;
+
+    if (first == "dir") {
+    } else if (first == "$") {
+      assert(second == "cd");
+      string dirname;
+      iss >> dirname;
+      root->child.push_back(parse(dirname));
+    } else {
+      int size = stoi(first);
+      root->child.push_back(new Node(second, size));
+    }
+  }
+  return root;
+}
+
+void print(Node *root, int indent = 0)
+{
+  for (int i = 0; i < indent; i++) {
+    cout << ' ';
+  }
+  root->print();
+  cout << endl;
+  for (auto c : root->child) {
+    print(c, indent+2);
+  }
+}
+
+int total_space = 70000000;
+int need = 30000000;
+int used_space, free_space;
+long long final_ans;
+
+int total_size(Node *root)
+{
+  if (!root->is_dir) {
+    return root->size;
+  }
+
+  long long ans = 0;
+  for (auto c : root->child) {
+    ans += total_size(c);
+  }
+
+  if (free_space + ans >= need) {
+    final_ans = min(final_ans, ans);
+  }
+
+  return ans;
+}
+
+int main()
+{
+  string line;
+  getline(cin, line);
+  Node *root = parse();
+  used_space = total_size(root);
+  free_space = total_space - used_space;
+
+  final_ans = LLONG_MAX;
+  total_size(root);
+  cout << final_ans << endl;
+
+  delete root;
+  return 0;
+}
