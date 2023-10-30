2d7e43cfd8c2ab3d1297a131865a24de1a5cab8f
Howard Cheng <howard.cheng@uleth.ca>
2023-10-30 11:44:14 -0600

    squarerooms

--- kattis/squarerooms.cc
+++ kattis/squarerooms.cc
@@ -3,130 +3,116 @@
 using namespace std;
 
 int n, m;
-string G[100];
+char grid[100][100];
+char soln[100][100];
+int prefix_treasure[100][100];
 
-// number of treasures and rocks from (0,0) to (r,c)
-int n_t[100][100], n_r[100][100];
-
-void init()
+char label(int index)
 {
-  for (int r = 0; r < n; r++) {
-    for (int c = 0; c < m; c++) {
-      n_t[r][c] = (G[r][c] == '$');
-      n_r[r][c] = (G[r][c] == '#');
-      if (r > 0) {
-	n_t[r][c] += n_t[r-1][c];
-	n_r[r][c] += n_r[r-1][c];
-      }
-      if (c > 0) {
-	n_t[r][c] += n_t[r][c-1];
-	n_r[r][c] += n_r[r][c-1];
-      }
-      if (r > 0 && c > 0) {
-	n_t[r][c] -= n_t[r-1][c-1];
-	n_r[r][c] -= n_r[r-1][c-1];
-      }
-    }
+  if (index < 26) {
+    return 'A' + index;
+  } else {
+    return 'a' + index - 26;
   }
 }
 
-int count(int nn[100][100], int r1, int c1, int r2, int c2)
+int num_treasure(int r, int c, int s)
 {
-  int ans = nn[r2][c2];
-  if (r1 > 0) {
-    ans -= nn[r1-1][c2];
+  int r2 = r+s-1, c2 = c+s-1;
+  int ans = prefix_treasure[r2][c2];
+  if (r-1 >= 0) {
+    ans -= prefix_treasure[r-1][c2];
   }
-  if (c1 > 0) {
-    ans -= nn[r2][c1-1];
+  if (c-1 >= 0) {
+    ans -= prefix_treasure[r2][c-1];
   }
-  if (r1 > 0 && c1 > 0) {
-    ans += nn[r1-1][c1-1];
+  if (r-1 >= 0 && c-1 >= 0) {
+    ans += prefix_treasure[r-1][c-1];
   }
+
   return ans;
 }
 
-char room_label(int k)
-{
-  if (k < 26) {
-    return 'A' + k;
-  } else {
-    return 'a' + (k-26);
-  }
-}
-
-string room[100];
-
-bool search(int r, int c, int room_idx = 0)
+bool solve(int r, int c, int curr)
 {
   if (r >= n) {
-    for (int i = 0; i < n; i++) {
-      cout << room[i] << endl;
-    }
     return true;
   }
   if (c >= m) {
-    return search(r+1, 0, room_idx);
+    return solve(r+1, 0, curr);
   }
-  if (!(room[r][c] == '.' || room[r][c] == '$')) {
-    return search(r, c+1, room_idx);
+  if (grid[r][c] == '#') {
+    soln[r][c] = '#';
+    return solve(r, c+1, curr);
+  }
+  if (soln[r][c] != 0) {
+    return solve(r, c+1, curr);
   }
 
-  for (int s = 1; s <= 100; s++) {
-    int r2 = r + s - 1, c2 = c + s - 1;
-    if (r2 >= n || c2 >= m)
-      break;
+  for (int s = 1; r+s-1 < n && c+s-1 < m; s++) {
+    int t = num_treasure(r, c, s);
+    if (t == 0) continue;
+    if (t > 1) break;
 
-    // has rock, can't continue
-    if (count(n_r, r, c, r2, c2) > 0) 
-      break;
-    int T = count(n_t, r, c, r2, c2);
-    if (T == 0) continue;
-    if (T > 1) break;
-
-    bool good = true;
-    for (int i = r; i <= r2; i++) {
-      for (int j = c; j <= c2; j++) {
-	good &= (room[i][j] == '.' || room[i][j] == '$');
+    int r2 = r+s-1, c2 = c+s-1;
+    bool empty = true;
+    for (int r1 = r; r1 <= r2; r1++) {
+      for (int c1 = c; c1 <= c2; c1++) {
+	empty &= (soln[r1][c1] == 0 && grid[r1][c1] != '#');
       }
     }
-
-    if (!good) continue;
+    if (!empty) continue;
     
-    for (int i = r; i <= r2; i++) {
-      for (int j = c; j <= c2; j++) {
-	room[i][j] = room_label(room_idx);
+    for (int r1 = r; r1 <= r2; r1++) {
+      for (int c1 = c; c1 <= c2; c1++) {
+	soln[r1][c1] = label(curr);
       }
     }
 
-    if (search(r, c2+1, room_idx+1)) {
+    if (solve(r, c+1, curr+1)) {
       return true;
     }
     
-    for (int i = r; i <= r2; i++) {
-      for (int j = c; j <= c2; j++) {
-	room[i][j] = '.';
+    for (int r1 = r; r1 <= r2; r1++) {
+      for (int c1 = c; c1 <= c2; c1++) {
+	soln[r1][c1] = 0;
       }
     }
   }
-
+  
   return false;
 }
 
 int main()
 {
   cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    cin >> G[i];
+  for (int r = 0; r < n; r++) {
+    for (int c = 0; c < m; c++) {
+      cin >> grid[r][c];
+
+      prefix_treasure[r][c] = (grid[r][c] == '$' ? 1 : 0);
+      if (r-1 >= 0) {
+	prefix_treasure[r][c] += prefix_treasure[r-1][c];
+      }
+      if (c-1 >= 0) {
+	prefix_treasure[r][c] += prefix_treasure[r][c-1];
+      }
+      if (r-1 >= 0 && c-1 >= 0) {
+	prefix_treasure[r][c] -= prefix_treasure[r-1][c-1];
+      }
+    }
   }
 
-  init();
-
-  for (int i = 0; i < n; i++) {
-    room[i] = G[i];
-  }
-  if (!search(0,0)) {
+  if (solve(0, 0, 0)) {
+    for (int r = 0; r < n; r++) {
+      for (int c = 0; c < m; c++) {
+	cout << soln[r][c];
+      }
+      cout << endl;
+    }
+  } else {
     cout << "elgnatcer" << endl;
   }
-  
+
   return 0;
 }
