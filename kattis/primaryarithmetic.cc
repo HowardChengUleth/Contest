529bfdbc1c12749c8c475ad2c9ae958324e3648b
Howard Cheng <howard.cheng@uleth.ca>
2024-07-10 10:22:27 -0600

    primaryarithmetic

--- kattis/primaryarithmetic.cc
+++ kattis/primaryarithmetic.cc
@@ -2,43 +2,44 @@
 
 using namespace std;
 
-bool solve()
+void solve(const string &s1, const string &s2)
 {
-  string s1, s2;
-  cin >> s1 >> s2;
-  if (s1 == "0" && s2 == "0")
-    return false;
+  int A1[11] = {0}, A2[11] = {0};
 
-  reverse(begin(s1), end(s1));
-  reverse(begin(s2), end(s2));
+  int len1 = 0, len2 = 0;
+  for (auto c : s1) {
+    A1[len1++] = c - '0';
+  }
+  for (auto c : s2) {
+    A2[len2++] = c - '0';
+  }
+  reverse(A1, A1+len1);
+  reverse(A2, A2+len2);
 
-  int num_carry = 0;
+  int ans = 0;
   int carry = 0;
-  for (int i = 0; i < s1.length() || i < s2.length(); i++) {
-    int d1 = (i < s1.length()) ? s1[i] - '0' : 0;
-    int d2 = (i < s2.length()) ? s2[i] - '0' : 0;
-    int sum = d1 + d2 + carry;
-    num_carry += (carry = (sum >= 10));
+  for (int i = 0; i < 10; i++) {
+    int sum = A1[i] + A2[i] + carry;
+    if (carry = (sum >= 10)) {
+      ans++;
+    }
   }
 
-  if (num_carry == 0) {
-    cout << "No ";
+  if (ans == 0) {
+    cout << "No carry operation." << endl;
+  } else if (ans == 1) {
+    cout << "1 carry operation." << endl;
   } else {
-    cout << num_carry << ' ';
+    cout << ans << " carry operations." << endl;
   }
-  cout << "carry operation";
-  if (num_carry > 1) {
-    cout << "s";
-  }
-  cout << "." << endl;
-  
-  return true;
 }
 
 int main()
 {
-  while (solve())
-    ;
+  string s1, s2;
+  while (cin >> s1 >> s2 && !(s1 == "0" && s2 == "0")) {
+    solve(s1, s2);
+  }
 
   return 0;
 }
