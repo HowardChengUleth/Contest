c3724bd552358842cc2f1336628938a06b630751
Howard Cheng <howard.cheng@uleth.ca>
2024-09-27 22:45:23 -0600

    quickestimate

---
+++ kattis/quickestimate.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  while (N--) {
+    string s;
+    cin >> s;
+    cout << s.length() << endl;
+  }
+  return 0;
+}
