7ae57524e3be60dcfe9f0bf8fb444837027a7aec
Howard Cheng <howard.cheng@uleth.ca>
2025-04-04 12:05:16 -0600

    goatrope

--- kattis/goatrope.cc
+++ kattis/goatrope.cc
@@ -2,23 +2,17 @@
 
 using namespace std;
 
-double dist(double x1, double y1, double x2, double y2)
+// given horizontal line (x1, ly) - (x2, ly), how far is it from (x,y)?
+double dist(int x, int y, int x1, int x2, int ly)
 {
-  return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-}
-
-double dist(double x, double y, double x1, double y1, double x2, double y2)
-{
-  double ux = x2-x1, uy = y2-y1;
-  double vx = x-x1, vy = y-y1;
-
-  double unorm = sqrt(ux*ux + uy*uy);
-  double dot = (ux*vx + uy*vy) / unorm;
-  if (0 <= dot && dot <= unorm) {
-    return sqrt((vx*vx + vy*vy) - dot*dot);
-  } else {
-    return min(dist(x,y,x1,y1), dist(x,y,x2,y2));
+  if (x1 <= x && x <= x2) {
+    return abs(y - ly);
   }
+
+  double dist1 = sqrt((x-x1)*(x-x1) + (y-ly)*(y-ly));
+  double dist2 = sqrt((x-x2)*(x-x2) + (y-ly)*(y-ly));
+
+  return min(dist1, dist2);
 }
 
 int main()
@@ -26,13 +20,14 @@ int main()
   int x, y, x1, y1, x2, y2;
   cin >> x >> y >> x1 >> y1 >> x2 >> y2;
 
-  double ans = 1e10;
-  ans = min(ans, dist(x, y, x1, y1, x1, y2));
-  ans = min(ans, dist(x, y, x1, y1, x2, y1));
-  ans = min(ans, dist(x, y, x2, y1, x2, y2));
-  ans = min(ans, dist(x, y, x1, y2, x2, y2));
+  double dist1 = dist(x, y, x1, x2, y1);
+  double dist2 = dist(x, y, x1, x2, y2);
+  double dist3 = dist(y, x, y1, y2, x1);
+  double dist4 = dist(y, x, y1, y2, x2);
 
+  double ans = min(min(dist1, dist2), min(dist3, dist4));
   cout << fixed << setprecision(10) << ans << endl;
-  
+
+
   return 0;
 }
