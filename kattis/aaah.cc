cbe8fc9861939b3abf84e6f3f5f4285eae66da1e
Howard Cheng <howard.cheng@uleth.ca>
2025-04-30 19:16:16 -0600

    aaah

---
+++ kattis/aaah.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string s1, s2;
+  cin >> s1 >> s2;
+  if (s1.length() >= s2.length()) {
+    cout << "go" << endl;
+  } else {
+    cout << "no" << endl;
+  }
+
+  return 0;
+}
