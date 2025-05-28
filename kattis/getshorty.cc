7f60d40be3d18d9f0753c40d6c7775d161268c1e
Howard Cheng <howard.cheng@uleth.ca>
2025-05-28 11:09:27 -0600

    getshorty

---
+++ kattis/getshorty.cc
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+/*
+ * Dijkstra's Algorithm for sparse graphs
+ *
+ * Author: Howard Cheng
+ *
+ * Given a weight matrix representing a graph and a source vertex, this
+ * algorithm computes the shortest distance, as well as path, to each
+ * of the other vertices.  The paths are represented by an inverted list,
+ * such that if v preceeds immediately before w in a path from the
+ * source to vertex w, then the path P[w] is v.  The distances from
+ * the source to v is given in D[v] (-1 if not connected).
+ *
+ * Call get_path to recover the path.
+ *
+ * Note: Dijkstra's algorithm only works if all weight edges are
+ *       non-negative.
+ *
+ * This version works well if the graph is not dense.  The complexity
+ * is O((n + m) log (n + m)) where n is the number of vertices and
+ * m is the number of edges.
+ *
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <queue>
+
+using namespace std;
+
+
+struct Edge {
+  int to;
+  double weight;       // can be double or other numeric type
+  Edge(int t, double w)
+    : to(t), weight(w) { }
+};
+  
+typedef vector<Edge>::iterator EdgeIter;
+
+struct Graph {
+  vector<Edge> *nbr;
+  int num_nodes;
+  Graph(int n)
+    : num_nodes(n)
+  {
+    nbr = new vector<Edge>[num_nodes];
+  }
+
+  ~Graph()
+  {
+    delete[] nbr;
+  }
+
+  // note: There is no check on duplicate edge, so it is possible to
+  // add multiple edges between two vertices
+  //
+  // If this is an undirected graph, be sure to add an edge both
+  // ways
+  void add_edge(int u, int v, double weight)
+  {
+    nbr[u].push_back(Edge(v, weight));
+  }
+};
+
+/* assume that D and P have been allocated */
+void dijkstra(const Graph &G, int src, vector<double> &D, vector<int> &P)
+{
+  typedef pair<double,int> pii;
+
+  int n = G.num_nodes;
+  vector<bool> used(n, false);
+  priority_queue<pii, vector<pii>,  greater<pii> > fringe;
+
+  D.resize(n);
+  P.resize(n);
+  fill(D.begin(), D.end(), -1);
+  fill(P.begin(), P.end(), -1);
+
+  D[src] = 0;
+  fringe.push(make_pair(D[src], src));
+
+  while (!fringe.empty()) {
+    pii next = fringe.top();
+    fringe.pop();
+    int u = next.second;
+    if (used[u]) continue;
+    used[u] = true;
+
+    for (EdgeIter it = G.nbr[u].begin(); it != G.nbr[u].end(); ++it) {
+      int v = it->to;
+      double weight = it->weight + next.first;
+      if (used[v]) continue;
+      if (D[v] == -1 || weight < D[v]) {
+	D[v] = weight;
+	P[v] = u;
+	fringe.push(make_pair(D[v], v));
+      }
+    }
+  }
+}
+
+int get_path(int v, const vector<int> &P, vector<int> &path)
+{
+  path.clear();
+  path.push_back(v);
+  while (P[v] != -1) {
+    v = P[v];
+    path.push_back(v);
+  }
+  reverse(path.begin(), path.end());
+  return path.size();
+}
+
+bool solve()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n == 0 && m == 0) {
+	return false;
+    }
+    
+    Graph G(n);
+    for (int i = 0; i < m; i++) {
+	int x, y;
+	double f;
+	cin >> x >> y >> f;
+	G.add_edge(x, y, -log(f));
+	G.add_edge(y, x, -log(f));
+    }
+
+    vector<double> D;
+    vector<int> P;
+    dijkstra(G, 0, D, P);
+
+    cout << fixed << setprecision(4) << exp(-D[n-1]) << endl;
+    
+
+    return true;
+}
+
+int main()
+{
+    while (solve())
+	;
+
+    return 0;
+}
