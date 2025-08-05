f12e3c5c958d7f1d9c1aa8c8b8b3a569c1d86807
Howard Cheng <howard.cheng@uleth.ca>
2025-08-05 14:24:54 -0600

    reduplikation

---
+++ kattis/reduplikation.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string w;
+    int n;
+
+    cin >> w >> n;
+    for (int i = 0; i < n; i++) {
+	cout << w;
+    }
+    cout << endl;
+
+    return 0;
+}
