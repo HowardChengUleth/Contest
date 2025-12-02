014dcf72e75210fe3c2d43696a12c00e85bd5314
Howard Cheng <howard.cheng@uleth.ca>
2025-12-01 22:14:42 -0700

    AOC 2025 Day 2

---
+++ adventofcode/2025/2a.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int64_t> find_invalid(int64_t start, int64_t end)
+{
+  vector<int64_t> v;
+  
+  for (int i = 1; i <= 999999; i++) {
+    ostringstream oss;
+    oss << i;
+    string s = oss.str() + oss.str();
+    int64_t x = stoll(s);
+    if (start <= x && x <= end) {
+      v.push_back(x);
+    }
+    if (x > end) {
+      break;
+    }
+  }
+  return v;
+}
+
+int main()
+{
+  string line;
+  cin >> line;
+
+  replace(begin(line), end(line), ',', ' ');
+  replace(begin(line), end(line), '-', ' ');
+
+  istringstream iss(line);
+  int64_t start, end;
+
+  int64_t ans = 0;
+  while (iss >> start >> end) {
+    auto ids = find_invalid(start, end);
+
+    cout << start << ' ' << end << ": ";
+    for (auto x : ids) {
+      cout << x << ' ';
+      ans += x;
+    }
+    cout << endl;
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
