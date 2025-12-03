54a77a716a143026f8b753d775462ee85af31a5e
Howard Cheng <howard.cheng@uleth.ca>
2025-12-02 21:47:25 -0700

    added template for AOC scripts

---
+++ adventofcode/2025/template.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int M, N;
+
+bool inside(int r, int c)
+{
+  return 0 <= r && r < M && 0 <= c && c < N;
+}
+
+const int dr[] = { -1, 1, 0, 0 };
+const int dc[] = { 0, 0, -1, 1 };
+
+int main()
+{
+
+
+  return 0;
+}
